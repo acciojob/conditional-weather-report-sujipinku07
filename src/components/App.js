8fd64061770921114ebc9abe51fd43d1f203555c
sujipinku07 <sujipinku07@gmail.com>
2024-12-08 13:24:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,27 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      renderBall: false, // To determine whether to show the ball or the button
-      ballPosition: { left: "0px" }, // CSS for the ball's position
-    };
-    this.buttonClickHandler = this.buttonClickHandler.bind(this);
-    this.moveBall = this.moveBall.bind(this);
-  }
 
-  // Handler for Start button
-  buttonClickHandler() {
-    this.setState({ renderBall: true }); // Show the ball
-  }
+import React, { useEffect, useState } from "react";
+import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
-  // Method to move the ball when ArrowRight is pressed
-  moveBall(event) {
-    if (event.key === "ArrowRight" || event.keyCode === 39) {
-      this.setState((prevState) => {
-        const currentLeft = parseInt(prevState.ballPosition.left, 10); // Extract current left value
-        const newLeft = currentLeft + 5; // Move 5px to the right
-        return { ballPosition: { left: `${newLeft}px` } };
-      });
-    }
-  }
 
-  // Add event listener when component mounts
-  componentDidMount() {
-    window.addEventListener("keydown", this.moveBall);
-  }
+const def = {temperature : 0, conditions : "Cool"}
 
-  // Remove event listener when component unmounts
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.moveBall);
-  }
+const App = () => {
 
-  // Conditionally render the Start button or the ball
-  renderBallOrButton() {
-    if (this.state.renderBall) {
-      return <div className="ball" style={this.state.ballPosition}></div>;
-    } else {
-      return <button className="start" onClick={this.buttonClickHandler}>Start</button>;
-    }
-  }
+  const[info, setInfo] = useState(def)
 
-  render() {
-    return (
-      <div className="playground">
-        {this.renderBallOrButton()}
-      </div>
-    );
-  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setInfo({ temperature: 25, conditions: "Sunny" })
+    }, 1000)
+  })
+
+  return (
+    <div>
+      <WeatherDisplay info={info} />
+    </div>
+  )
 }
 
 export default App;
